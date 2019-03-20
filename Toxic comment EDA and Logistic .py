a456225c64841c0c8b525fc01d9de88b033c6a36
aakash-sheth <36289336+aakash-sheth@users.noreply.github.com>
2019-03-20 11:08:05 -0400

    Update Toxic comment EDA and Logistic .py
    Modifications

--- Toxic comment EDA and Logistic .py
+++ Toxic comment EDA and Logistic .py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 
 import pandas as pd
 import seaborn as sns
 import nltk
 import numpy as np
-
-
-# In[ ]:
-
 
 #viz
 import matplotlib.pyplot as plt
@@ -31,10 +22,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.model_selection import train_test_split,cross_val_score
 
-
-# In[ ]:
-
-
 #settings
 color = sns.color_palette()
 sns.set_style("dark")
@@ -42,48 +29,19 @@ eng_stopwords = set(stopwords.words("english"))
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[ ]:
-
-
 #reading csv file
 train=pd.read_csv('train.csv')
 test=pd.read_csv('test.csv')
 train.tail(10)
 
-
-# In[ ]:
-
-
 #data introspection
 print(len(test))
 
-
-# In[ ]:
-
-
 train.info()
-
-
-# In[ ]:
-
 
 x=train.iloc[:,2:7].sum()
 
-
-# In[ ]:
-
-
-x
-
-
-# In[ ]:
-
-
 rowsums=train.iloc[:,2:7].sum(axis=1)
-
-
-# In[ ]:
-
 
 train['clean']=(rowsums==0)
 train['clean'].sum()
@@ -92,18 +50,11 @@ print("Total clean comments = ",train['clean'].sum())
 print("Total tags =",x.sum())
 
 
-# In[ ]:
-
-
 print("Check for missing values in comment dataset")
 null_check=train.isnull().sum()
 print(null_check)
 print("filling NA with \"unknown\"")
 train["comment_text"].fillna("unknown", inplace=True)
-
-
-# In[ ]:
-
 
 x=train.iloc[:,2:].sum()
 #plot
@@ -120,10 +71,6 @@ for rect, label in zip(rects, labels):
     ax.text(rect.get_x() + rect.get_width()/2, height + 5, label, ha='center', va='bottom')
 
 plt.show()
-
-
-# In[ ]:
-
 
 x=rowsums.value_counts()
 
@@ -143,10 +90,6 @@ for rect, label in zip(rects, labels):
 
 plt.show()
 
-
-# In[ ]:
-
-
 temp_df=train.iloc[:,2:-1]
 # filter temp by removing clean comments
 # temp_df=temp_df[~train.clean]
@@ -156,10 +99,6 @@ plt.figure(figsize=(10,8))
 sns.heatmap(corr,
             xticklabels=corr.columns.values,
             yticklabels=corr.columns.values, annot=True)
-
-
-# In[ ]:
-
 
 #Crosstab
 # Since technically a crosstab between all 6 classes is impossible to vizualize, lets take a 
@@ -173,10 +112,6 @@ out = pd.concat(corr_mats,axis=1,keys=temp_df.columns[1:])
 
 out
 
-
-# In[ ]:
-
-
 #Checking for Toxic and Severe toxic for now
 col1="toxic"
 col2="severe_toxic"
@@ -186,23 +121,11 @@ print(confusion_matrix)
 #new_corr=cramers_corrected_stat()
 #print("The correlation between Toxic and Severe toxic using Cramer's stat=",new_corr)
 
-
-# In[ ]:
-
-
 print("toxic:")
 print(train[train.severe_toxic==1].iloc[3,1])
 
-
-# In[ ]:
-
-
 print("severe_toxic:")
 print(train[train.severe_toxic==1].iloc[8,1])
-
-
-# In[ ]:
-
 
 print("Threat:")
 print(train[train.threat==1].iloc[1,1])
@@ -215,24 +138,13 @@ print("Obscene:")
 print(train[train.obscene==1].iloc[2,1])
 
 
-# In[ ]:
-
-
 print("identity_hate:")
 print(train[train.identity_hate==1].iloc[4,1])
-
-
-# In[ ]:
-
 
 subset=train[train.toxic==1]
 text=subset.comment_text.values
 
 stopword=set(STOPWORDS)
-
-
-# In[ ]:
-
 
 plt.figure(figsize=(20,10))
 wordcloud=WordCloud(background_color='WHITE',mode="RGB",width=500,height=400,max_words=1000,stopwords=stopword)
@@ -240,10 +152,6 @@ wordcloud.generate("".join(text))
 plt.imshow(wordcloud.recolor(colormap='Blues',random_state=240))
 plt.axis("off")
 plt.title("Frequent words in Toxic comments")
-
-
-# In[ ]:
-
 
 #Severely toxic comments
 plt.subplot(222)
@@ -255,10 +163,6 @@ wc.generate(" ".join(text2))
 plt.axis("off")
 plt.title("Frequent words in Severe Toxic Comments", fontsize=16)
 plt.imshow(wc.recolor(colormap= 'Reds' , random_state=244))
-
-
-# In[ ]:
-
 
 #Obscence comments comments
 plt.subplot(222)
@@ -273,10 +177,6 @@ plt.axis("off")
 plt.title("Words frequented in Obscence", fontsize=16)
 plt.imshow(wc.recolor(colormap= 'Reds' , random_state=244))
 
-
-# In[ ]:
-
-
 #Identity hate
 plt.subplot(222)
 plt.figure(figsize=(20,10))
@@ -290,10 +190,6 @@ plt.axis("off")
 plt.title("Frequent word in identity hate comments", fontsize=16)
 plt.imshow(wc.recolor(colormap= 'Reds' , random_state=244))
 
-
-# In[ ]:
-
-
 plt.subplot(222)
 plt.figure(figsize=(20,10))
 #severe_toxic_mask=np.array(Image.open("severe toxic.jpg"))
@@ -305,9 +201,6 @@ wc.generate(" ".join(text2))
 plt.axis("off")
 plt.title("Frequent words in threat comment", fontsize=16)
 plt.imshow(wc.recolor(colormap= 'RdYlBu' , random_state=244))
-
-
-# In[ ]:
 
 
 plt.subplot(222)
@@ -324,10 +217,6 @@ plt.imshow(wc.recolor(colormap= 'Greens' , random_state=244))
 
 
 # **Text Processing**
-# 
-
-# In[ ]:
-
 
 def text_process(mess):
     """
@@ -345,31 +234,18 @@ def text_process(mess):
     # Now just remove any stopwords
     return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
 
-
-# In[ ]:
-
-
 train['comment_text'].apply(text_process)
 
 
 #  ### Logestic regression for classification model
 
-# In[ ]:
-
 
 from sklearn.feature_extraction.text import CountVectorizer
-
-
-# In[ ]:
 
 
 train_text=train['comment_text']
 test_text=test['comment_text']
 all_text=pd.concat([train_text,test_text],axis=0)
-
-
-# In[ ]:
-
 
 word_vectorizer= TfidfVectorizer(
 sublinear_tf=True,
@@ -380,20 +256,10 @@ max_features=10000
 )
 
 
-# In[ ]:
-
-
 word_vectorizer.fit(all_text)
-
-
-# In[ ]:
-
 
 train_word_features=word_vectorizer.transform(train_text)
 test_word_features=word_vectorizer.transform(test_text)
-
-
-# In[ ]:
 
 
 char_vectorizer=TfidfVectorizer(
@@ -406,20 +272,10 @@ max_features=50000
 )
 
 
-# In[ ]:
-
-
 char_vectorizer.fit(all_text)
-
-
-# In[ ]:
-
 
 train_char_features=char_vectorizer.transform(train_text)
 test_char_features=char_vectorizer.transform(test_text)
-
-
-# In[ ]:
 
 
 from scipy.sparse import hstack
@@ -427,22 +283,13 @@ train_features = hstack([train_char_features, train_word_features])
 test_features = hstack([test_char_features, test_word_features])
 
 
-# In[ ]:
-
-
 scores = []
 submission = pd.DataFrame.from_dict({'id': test['id']})
-
-
-# In[ ]:
 
 
 optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy']
-
-
-# In[ ]:
 
 
 for class_name in class_names:
@@ -457,19 +304,9 @@ for class_name in class_names:
     submission[class_name] = classifier.predict_proba(test_features)[:, 1]
 
 
-# In[ ]:
-
-
 print('Total CV score is {}'.format(np.mean(scores)))
 
-
-# In[ ]:
-
-
 submission.to_csv('submission.csv', index=False)
-
-
-# In[ ]:
 
 
 
